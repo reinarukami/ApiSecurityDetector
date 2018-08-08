cd45f0898d6af76c3e41e5f321f4e54432f80976
Robin <robin.sumalde.ivp@gmail.com>
2018-08-08 20:11:52 +0800

    Initial Commit
    Initial Commit

---
+++ ApiSecurityDetector/Controllers/ValidateController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiSecurityDetector.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSecurityDetector.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValidateController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<bool> Get()
+        {
+			var result = ValidateFlashDrive._GetFlashDrives();
+			return result;
+        }
+
+		[HttpPost]
+		public ActionResult<bool> Post()
+		{
+			//Validate[FromBody] USB Serial and Settings if it is Registered in the record
+
+			return true;
+		}
+
+
+    }
+}
---
+++ ApiSecurityDetector/Services/ValidateFlashDrive.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiSecurityDetector.Services
+{
+    public class ValidateFlashDrive
+    {
+		public static bool _GetFlashDrives()
+		{
+			DriveInfo[] mydrives = DriveInfo.GetDrives();
+
+			foreach (DriveInfo mydrive in mydrives)
+			{
+				if (mydrive.DriveType == DriveType.Removable)
+				{
+					if (mydrive.IsReady == true)
+					{
+						if (mydrive.VolumeLabel == "WALLETHOST")
+						{
+							//Codes for Read WalletConfiguration.json to validate USB HashKey , and Validate USB Configuration
+
+
+
+							return true;
+
+						}
+
+						continue;
+					}
+
+					continue;
+
+				}
+
+				continue;
+			}
+
+			return false;
+		}
+
+
+	}
+}
